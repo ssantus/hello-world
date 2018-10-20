4017f2663d9428484bcade58b516fc6e668ecbae
ssantus <34136348+ssantus@users.noreply.github.com>
2018-10-21 00:05:55 +0200

    Update hello.py
    Updating modifications from Spyder to experimental branch

--- hello.py
+++ hello.py
@@ -2,4 +2,6 @@ print ('Hello world')
 print ('Everything is working lul')
 
 print('this is written in the experimental branch')
+
+print ('What happens if i edit from Spyder (no direct connection to github desktop')
 input()
